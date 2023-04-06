7e77fcd3d9be303a3e84fad9052b8cbf3af1c2ca
ayushmandixit2000 <ayushmandixit2000@gmail.com>
2023-04-06 19:33:45 +0800

    Update transfer.py

--- minting/transfer.py
+++ minting/transfer.py
@@ -2,17 +2,51 @@ from algosdk import account, transaction
 from algosdk.v2client import algod
 from algosdk import mnemonic
 
-sender_mnemonic = "opera track ladder fury gospel merit citizen glass window assist citizen winner sound actress thunder base lock cheap axis vanish impact lobster distance abstract crane"
-receiver = "A6PBGDL3XFYJT3GLWOS2W7QCUA33BP5IO3AVKLJOZ4LUKRNNTP2PQWQGCA"
+
+# Create a new algod client, configured to connect to our local sandbox
+algod_address = "http://localhost:4001"
+algod_token = "a" * 64
+algod_client = algod.AlgodClient(algod_token, algod_address)
+
+sender_mnemonic = "mom lottery uniform olive visa occur garlic artefact minimum reward custom legend suit stock install leg doctor favorite retreat cart all exact camp able cute"
+
+
+print ("░██████╗██╗░░░██╗██████╗░██████╗░██╗░░░░░██╗███████╗██████╗░")
+print("██╔════╝██║░░░██║██╔══██╗██╔══██╗██║░░░░░██║██╔════╝██╔══██╗")
+print("╚█████╗░██║░░░██║██████╔╝██████╔╝██║░░░░░██║█████╗░░██████╔╝")
+print("░╚═══██╗██║░░░██║██╔═══╝░██╔═══╝░██║░░░░░██║██╔══╝░░██╔══██╗")
+print("██████╔╝╚██████╔╝██║░░░░░██║░░░░░███████╗██║███████╗██║░░██║")
+print("╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝░░░░░╚══════╝╚═╝╚══════╝╚═╝░░╚═╝")
+
+print("████████╗██████╗░░█████╗░███╗░░██╗░██████╗░█████╗░░█████╗░████████╗██╗░█████╗░███╗░░██╗")
+print("╚══██╔══╝██╔══██╗██╔══██╗████╗░██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║")
+print("░░░██║░░░██████╔╝███████║██╔██╗██║╚█████╗░███████║██║░░╚═╝░░░██║░░░██║██║░░██║██╔██╗██║")
+print("░░░██║░░░██╔══██╗██╔══██║██║╚████║░╚═══██╗██╔══██║██║░░██╗░░░██║░░░██║██║░░██║██║╚████║")
+print("░░░██║░░░██║░░██║██║░░██║██║░╚███║██████╔╝██║░░██║╚█████╔╝░░░██║░░░██║╚█████╔╝██║░╚███║")
+print("░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝")
+
+print("")
+print("")
+print("")
+
+print("Enter supplier's public address to transfer to: ")
+print("")
+print("")
+
+receiver = input()
+# receiver = "ZVOU4DSUBM74XKLLPW4AUCUG2E7BJ2AQVE7JJCUUFWLLJ5NKW6DQHSXBNM"
 
 def charity_to_supplier(sender_mnemonic,receiver):
     # Your Algorand node settings (replace with your own values)
     ALGOD_ADDRESS = "http://localhost:4001"
-    ALGOD_TOKEN = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    ALGOD_TOKEN = "a" * 64
 
     # Sender and receiver addresses (replace with your own values)
 
     approved_wallets = ["A6PBGDL3XFYJT3GLWOS2W7QCUA33BP5IO3AVKLJOZ4LUKRNNTP2PQWQGCA"]
+
+
+    approved_wallets1 = ["S5EEOYBI6FDZT6AF6O342CJEMX3JOO5J2KLX6ST3JOGKDKMBYGDHZYJA6E"] #to show if wallet not approved
 
 
     if receiver not in approved_wallets:
@@ -29,7 +63,9 @@ def charity_to_supplier(sender_mnemonic,receiver):
         params = algod_client.suggested_params()
 
         # Define the note (message) as a string
-        message = "50 USD sent for the purchase of food to be sent for Turkiye Earthquake relief"
+        # message = "50 USD sent for the purchase of food to be sent for Turkiye Earthquake relief"
+        message = input("Add message to be tied to this transaction: ")
+        print("")
 
         # Convert the message to bytes
         note = message.encode()
@@ -37,13 +73,16 @@ def charity_to_supplier(sender_mnemonic,receiver):
         print(note)
 
         # Create the transaction
-        txn = transaction.PaymentTxn(mnemonic.to_public_key(sender_mnemonic), params, receiver, 100000, None, note)
+        txn = transaction.PaymentTxn(account.address_from_private_key(mnemonic.to_private_key(sender_mnemonic)), params, receiver, 1000, None, note)
 
         # Sign the transaction
         signed_txn = txn.sign(sender_private_key)
 
         # Send the transaction
         txid = algod_client.send_transaction(signed_txn)
+        print("Sending...")
+        print("")
         print("Transaction ID:", txid)
 
 charity_to_supplier(sender_mnemonic,receiver)
+
